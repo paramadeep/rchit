a883b944c558b62252e34974278e73f5a52312d0
deepak <anandhadeepak@gmail.com>
2016-01-20 01:57:32 +0530

    added material icon

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # rchit
+
+
+
+## App Icon
+http://romannurik.github.io/AndroidAssetStudio/icons-launcher.html#foreground.type=clipart&foreground.space.trim=1&foreground.space.pad=0.05&foreground.clipart=res%2Fclipart%2Ficons%2Fimage_hdr_weak.svg&foreColor=ffc107%2C55&crop=1&backgroundShape=circle&backColor=673ab7%2C100&effects=shadow
--- app/components/screens/allChits.js
+++ app/components/screens/allChits.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var React = require('react-native');
+var MK = require('react-native-material-kit');
 var {
   Text,
   StyleSheet,
@@ -9,10 +10,16 @@ var {
   Image,
 } = React;
 
+var {
+  MKButton,
+  MKColor,
+} = MK;
+
 var Title = require('../parts/title');
 var ChitList = require('../parts/chitList')
 var chit=require('../../models/chit')
-
+const AccentColoredFab = MKButton.accentColoredFab()
+  .build();
 var AllChits = React.createClass({
   getInitialState() { 
     return {
@@ -21,12 +28,6 @@ var AllChits = React.createClass({
   },
   addChit(){
     this.props.navigator.push({screen:this.props.screens.addChit});
-  },
-  deleteChit(){
-    chit.deleteAll().then(   
-    this.setState({
-      chits: []
-    }))
   },
   fetchData(){
     chit.all().
@@ -45,31 +46,43 @@ var AllChits = React.createClass({
         <View style={styles.header}> 
           <Title value={'All chits'}/>
           <TouchableHighlight onPress={this.addChit}>
-            <Image source={require('../../icons/ic_add_black_24dp.png')} />
-          </TouchableHighlight>
-          <TouchableHighlight onPress={this.deleteChit}>
-            <Image source={require('../../icons/ic_add_circle_black_24dp.png')} />
+            <Image source={require('../../icons/ic_action_ic_add_circle_black_24dp.png')} />
           </TouchableHighlight>
         </View>
         <ChitList screens={this.props.screens} navigator={this.props.navigator} chits={this.state.chits}/>
+        <AccentColoredFab
+          backgroundColor={MKColor.Teal}
+          shadowRadius={2}
+          shadowOffset={{width:0, height:2}}
+          shadowOpacity={.7}
+          shadowColor="black"
+          onPress={() => {
+          console.log('hi, raised button!');
+          }}
+          >
+          <Text pointerEvents="none"
+            style={{color: 'white', fontWeight: 'bold',}}>
+            +
+          </Text>
+        </AccentColoredFab>
       </View>);
-},
-});
+        },
+        });
 
-var styles = StyleSheet.create({
-  container: {
-  },
-  header:{
-    flexDirection: 'row',
-    backgroundColor: '#2196f3',
-    height: 40,
-    flexWrap: 'nowrap',
-    alignItems:'center'
-  },
-  addButton:{
-    textAlign: "center"
-  }
-});
+        var styles = StyleSheet.create({
+          container: {
+          },
+          header:{
+            flexDirection: 'row',
+            backgroundColor: '#689F38',
+            height: 80,
+            flexWrap: 'nowrap',
+            alignItems:'center'
+          },
+          addButton:{
+            textAlign: "center"
+          }
+        });
 
 
-module.exports=AllChits;
+        module.exports=AllChits;
